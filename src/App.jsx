5ff1907fa57146d54b8c1e1586fe44d1034e61fc
Sergio Panigua <xpanisergio@gmail.com>
2023-05-06 13:11:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Map } from "./components/Map";
+import './App.scss';
+
+function App() {
+
+
+  return (
+    <div className="mapa-container">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+
+
+export const Map = () => {
+    return (
+        <>
+            <h1>Aplicacion funcionando!</h1>
+
+
+        </>
+    );
+};
